8c65de2d546581e98ab3999d03729affcd276218
Dylan Zucker <djz003.bucknell.edu>
2018-04-13 17:12:00 -0400

    Added to bar

--- mealtracker/src/TopBar.jsx
+++ mealtracker/src/TopBar.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { SelectField, MenuItem, FlatButton, AppBar } from 'material-ui';
+import { ToolbarGroup, SelectField, MenuItem, FlatButton, AppBar } from 'material-ui';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Menu from 'material-ui/svg-icons/navigation/menu';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+
+
+const Logged = (props) => (
+  <IconMenu
+    {...props}
+     iconButtonElement={
+      <IconButton><Menu color="white"/></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Refresh" />
+    <MenuItem primaryText="Help" />
+    <MenuItem primaryText="Sign out" />
+  </IconMenu>
+);
 
 
 class TopBar extends Component {
+  constructor(props){
+      super(props)
+      this.dropDown=this.dropDown.bind(this)
+    }
+    dropDown(e, obj) {
+
+    }
   render() {
+
     return (
-      <AppBar title="Mealest" iconClassNameRight="muidocs-icon-navigation-expand-more"
-/>
+       <AppBar title="Mealest" iconElementLeft={<Logged />} onLeftButtonClick={this.dropDown}/>
+
+      //<AppBar title="Mealest" iconElementRight={<ToolbarGroup>{myTabs}</ToolbarGroup>}/>
     );
   }
 }
