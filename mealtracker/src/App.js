453c9500d7278907cff20492832e291515580495
Dylan Zucker <djz003.bucknell.edu>
2018-04-13 22:46:38 -0400

    Some Dashboard stuff

--- mealtracker/src/App.js
+++ mealtracker/src/App.js
@@ -3,15 +3,14 @@ import logo from './logo.svg';
 import { SelectField, MenuItem, FlatButton, AppBar } from 'material-ui';
 import './App.css';
 import TopBar from './TopBar.jsx';
+import Dashboard from './Dashboard.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopBar/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Dashboard/>
       </div>
     );
   }
--- mealtracker/src/Dashboard.jsx
+++ mealtracker/src/Dashboard.jsx
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { ToolbarGroup, SelectField, MenuItem, FlatButton, AppBar } from 'material-ui';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import Menu from 'material-ui/svg-icons/navigation/menu';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
+import { SelectField, MenuItem, RaisedButton, FlatButton, AppBar } from 'material-ui';
+import DatePicker from 'material-ui/DatePicker';
+import {Container, Row, Col} from 'reactstrap';
+
 
 
 class Dashboard extends Component {
   constructor(props){
       super(props)
+      this.state={
+        date:this.getDate()
+      }
+      this.getDate=this.getDate.bind(this)
+      this.changeDate=this.changeDate.bind(this)
+
+      }
+    getDate() {
+      var today = new Date()
+      today.setFullYear(today.getFullYear())
+      return today
+
+    }
+    changeDate(e, newDate) {
+      this.setState({date:newDate})
 
     }
   render() {
     return (
-       
+      //Put Chosen days meals here with text fields Tables and buttons
+      <div>
 
+        <Container>
+        <Row>
+            <Col lg={3} md={6} sm={12}>.col</Col>
+            <Col lg={3} md={6} sm={12}>.col</Col>
+            <Col lg={3} md={6} sm={12}>.col</Col>
+            <Col lg={3} md={6} sm={12}>.col</Col>
+        </Row>
+          <Row>
+            <Col>
+              <DatePicker
+                value={this.state.date}
+                onChange={this.changeDate}
+                defaultDate={this.getDate()}
+                mode="landscape"
+              />
+            </Col>
+            <Col>
+              <RaisedButton> Add New Meal </RaisedButton>
+            </Col>
+
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
 
-export default TopBar;
+export default Dashboard;
